e9f6f3b356fdd99974872c076152b212d73330db
thu <mingfei.net@gmail.com>
2016-12-17 14:36:19 +0800

    Update

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -1,15 +1,14 @@
-DROP DATABASE IF EXISTS exam;
-CREATE DATABASE exam;
+CREATE DATABASE IF NOT EXISTS exam;
 
 # user
 DROP TABLE IF EXISTS exam.user;
-CREATE TABLE exam.user
-(
-  id       INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE exam.user (
+  id       INT AUTO_INCREMENT PRIMARY KEY,
   username VARCHAR(255),
   password VARCHAR(255)
 );
 
 INSERT INTO exam.user VALUES (NULL, 'a', 'a');
 
-USE exam;
+SELECT *
+FROM exam.user;
