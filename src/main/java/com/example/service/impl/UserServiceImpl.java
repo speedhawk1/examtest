ed87636634616cc423bb552b38a2401f57cc3cbb
thu <mingfei.net@gmail.com>
2016-12-18 11:18:36 +0800

    Update

--- src/main/java/com/example/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/service/impl/UserServiceImpl.java
@@ -3,33 +3,42 @@ package com.example.service.impl;
 import com.example.dao.GenericDao;
 import com.example.model.User;
 import com.example.service.UserService;
+import com.example.util.Constant;
 import org.apache.commons.codec.digest.DigestUtils;
+import org.jasypt.util.password.StrongPasswordEncryptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 /**
+ *
  * Created by Administrator on 2015/12/27.
  */
 @Service
 public class UserServiceImpl extends GenericServiceImpl<User, Integer> implements UserService {
+
+    private StrongPasswordEncryptor encryptor;
 
     @Override
     @Autowired
     @Qualifier("userDaoImpl")
     public void setGenericDao(GenericDao<User, Integer> genericDao) {
         super.genericDao = genericDao;
+        encryptor = new StrongPasswordEncryptor();
     }
 
     @Override
     public User login(User user) {
-        user.setPassword(DigestUtils.md5Hex(user.getPassword()));
+        String plainPassword = user.getPassword();
+        user.setPassword(null);
         user = genericDao.query(user);
         if (user != null) {
-            return user;
-        } else {
-            return null;
+            String encryptedPassword = user.getPassword();
+            if (encryptor.checkPassword(plainPassword, encryptedPassword)) {
+                return user;
+            }
         }
+        return null;
     }
 
     @Override
@@ -39,8 +48,10 @@ public class UserServiceImpl extends GenericServiceImpl<User, Integer> implement
         if (genericDao.query(user) != null) {
             return false;
         }
-        user.setPassword(DigestUtils.md5Hex(password));
+        user.setPassword(encryptor.encryptPassword(password));
         genericDao.add(user);
         return true;
     }
 }
+
+// salt
