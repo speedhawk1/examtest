7dd13768d5e37f6954e3d8e7c36dc904b36ac326
thu <mingfei.net@gmail.com>
2017-01-06 16:46:11 +0800

    Update

--- src/main/java/com/example/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/service/impl/UserServiceImpl.java
@@ -3,42 +3,28 @@ package com.example.service.impl;
 import com.example.dao.GenericDao;
 import com.example.model.User;
 import com.example.service.UserService;
-import com.example.util.Constant;
-import org.apache.commons.codec.digest.DigestUtils;
 import org.jasypt.util.password.StrongPasswordEncryptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 /**
- *
  * Created by Administrator on 2015/12/27.
  */
 @Service
 public class UserServiceImpl extends GenericServiceImpl<User, Integer> implements UserService {
-
-    private StrongPasswordEncryptor encryptor;
 
     @Override
     @Autowired
     @Qualifier("userDaoImpl")
     public void setGenericDao(GenericDao<User, Integer> genericDao) {
         super.genericDao = genericDao;
-        encryptor = new StrongPasswordEncryptor();
     }
 
     @Override
     public User login(User user) {
-        String plainPassword = user.getPassword();
         user.setPassword(null);
-        user = genericDao.query(user);
-        if (user != null) {
-            String encryptedPassword = user.getPassword();
-            if (encryptor.checkPassword(plainPassword, encryptedPassword)) {
-                return user;
-            }
-        }
-        return null;
+        return genericDao.query(user);
     }
 
     @Override
@@ -48,10 +34,8 @@ public class UserServiceImpl extends GenericServiceImpl<User, Integer> implement
         if (genericDao.query(user) != null) {
             return false;
         }
-        user.setPassword(encryptor.encryptPassword(password));
+        user.setPassword(password);
         genericDao.add(user);
         return true;
     }
 }
-
-// salt
